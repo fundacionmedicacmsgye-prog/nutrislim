de98b4359990643d20f5387aa271a45969469d12
fundacionmedicacmsgye-prog <fundacionmedicacmsgye@gmail.com>
2026-06-30 11:43:55 -0500

    Fix - clave Supabase via variable de entorno

---
+++ netlify/functions/get-config.js
@@ -0,0 +1,13 @@
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseAnonKey: process.env.SUPABASE_ANON_KEY
+    })
+  };
+};
